bc58b50e051c51e7b83b24c540a3365f7f0ddb71
Adrian Grimm <usmcamgrimm@gmail.com>
2021-09-29 06:34:48 -0400

    counterReducer state is undefined but set to 0 initially??

--- src/index.js
+++ src/index.js
@@ -4,45 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux'
+import allReducers from './reducers'
 
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
-
-//STORE - Globalized State
-
-
-
-//ACTION - Describes what you are going to do
-const increment = () => {
-  return {
-    type: 'INCREMENT'
-  }
-}
-const decrement = () => {
-  return {
-    type: 'DECREMENT'
-  }
-}
-
-
-//REDUCER
-const counter = (state = 0, action) => {
-  switch(action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-  }
-}
-
-let store = createStore(counter);
-
-//Display in console
-store.subscribe(() => console.log(store.getState()))
-
-
-//DISPATCH - Execute the action
-store.dispatch(increment()) // Displays 1
-store.dispatch(decrement()) // Back to 0
 
 ReactDOM.render(
   <React.StrictMode>

bf7bbc3198e5d06800fb734c8abfb53e551f5447
Adrian Grimm <usmcamgrimm@gmail.com>
2021-09-29 06:02:18 -0400

    Set up basic redux implementation to increment/decrement

--- src/index.js
+++ src/index.js
@@ -3,6 +3,46 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux'
+
+
+
+//STORE - Globalized State
+
+
+
+//ACTION - Describes what you are going to do
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+
+//REDUCER
+const counter = (state = 0, action) => {
+  switch(action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+}
+
+let store = createStore(counter);
+
+//Display in console
+store.subscribe(() => console.log(store.getState()))
+
+
+//DISPATCH - Execute the action
+store.dispatch(increment()) // Displays 1
+store.dispatch(decrement()) // Back to 0
 
 ReactDOM.render(
   <React.StrictMode>
